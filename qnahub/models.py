807a77b4af5865d52d3b42055ddbc246c94eef04
huzaius <huzaius@gmail.com>
2025-08-09 16:47:41 -0500

    Login and Register to DB

--- qnahub/models.py
+++ qnahub/models.py
@@ -1,10 +1,17 @@
 from datetime import datetime, timezone
-from qnahub import db  # Note: Consider moving db to its own file to avoid circular imports.
+from qnahub import db,login_manager
+from flask_login import UserMixin
 from sqlalchemy import func, select
 from sqlalchemy.ext.hybrid import hybrid_property
 
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
--- qnahub/routes.py
+++ qnahub/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template, request, url_for, redirect, flash
 from qnahub.forms import RegistrationForm, LoginForm
+from qnahub.models import User, Question, Answer, Vote
+from qnahub import app,db
+from qnahub import bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
 
-from qnahub import app
+
+
 
 
 
@@ -24,11 +29,18 @@ def question():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    #(future) Allow login for both username and email
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "huzaius@gmail.com" and form.password.data == "password":
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data) :
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
             flash(f"Account Login Successfully for {form.email.data}!", "success")
-            return redirect(url_for("index"))
+            return redirect(next_page) if next_page else redirect(url_for("index"))
+     
         else:
             flash("Login Unsuccessful. Please check email and password", "danger")
 
@@ -37,16 +49,26 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    
     form = RegistrationForm()
+    
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
+
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your account has been created! You are now able to log in", "success")
         return redirect(url_for("index"))
     return render_template("register.html", form=form)
 
 
 @app.route("/users")
+@login_required
 def users():
-    return render_template("users.html")
+    return render_template("users.html", title=current_user.username)
 
 
 @app.route("/subjects")
@@ -57,3 +79,19 @@ def subjects():
 @app.route("/unanswered")
 def unanswered():
     return render_template("unanswered.html")
+
+
+@app.route("/settings")
+@login_required
+def settings():
+    return render_template("settings.html")
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
